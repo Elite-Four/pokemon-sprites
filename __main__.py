b847a4de3dd9b363da6410e9d913c64e2cb83e82
George Cheng <Gerhut@GMail.com>
2020-05-07 07:24:28 +0000

    Add index.html

--- __main__.py
+++ __main__.py
@@ -4,37 +4,36 @@ from logging import getLogger, StreamHandler
 from os import path
 from sys import stderr
 
+from jinja2 import Environment, FileSystemLoader
 import requests
+
+base_dir = path.dirname(path.abspath(__file__))
 
 logger = getLogger(__name__)
 logger.setLevel('INFO')
 logger.addHandler(StreamHandler(stream=stderr))
 
-
-def _get_url(national_id, form_index):
-    if form_index == 0:
-        return ('https://assets.pokemon.com'
-                '/assets/cms2/img/pokedex/full/'
-                '%03d.png') % (national_id,)
-    else:
-        return ('https://assets.pokemon.com'
-                '/assets/cms2/img/pokedex/full/'
-                '%03d_f%d.png') % (national_id, form_index + 1)
+env = Environment(loader=FileSystemLoader(base_dir))
 
 
 def main():
-    base_dir = path.dirname(path.abspath(__file__))
     national_id = 1
     form_index = 0
+    images = []
 
     while True:
         if form_index == 0:
             sprite_id = '%d' % (national_id,)
+            url = ('https://assets.pokemon.com'
+                   '/assets/cms2/img/pokedex/full/'
+                   '%03d.png') % (national_id,)
         else:
             sprite_id = '%d-%d' % (national_id, form_index)
-        url = _get_url(national_id, form_index)
-        response = requests.get(url)
+            url = ('https://assets.pokemon.com'
+                   '/assets/cms2/img/pokedex/full/'
+                   '%03d_f%d.png') % (national_id, form_index + 1)
 
+        response = requests.get(url)
         if response.status_code != requests.codes.ok:
             logger.warning('%s fetch failed.', sprite_id)
             if form_index == 0:
@@ -48,9 +47,16 @@ def main():
         file_path = path.join(base_dir, 'docs/%s.png' % (sprite_id,))
         with open(file_path, 'wb') as f:
             f.write(response.content)
+        images.append('%s.png' % (sprite_id,))
+
         logger.info('%s saved.', file_path)
 
         form_index += 1
+
+    file_path = path.join(base_dir, 'docs/index.html')
+    template = env.get_template('index.jinja2')
+    with open(file_path, 'w') as f:
+        f.write(template.render(images=images))
 
 
 if __name__ == '__main__':
